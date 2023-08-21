d7b956d759ea227c6ffde50bac5e5ee6bd996845
Scott Nelson <scott@scottnelson.co>
2023-08-21 14:44:42 -0400

    Handle undefined table

--- sqlcritic/database/postgres.py
+++ sqlcritic/database/postgres.py
@@ -1,4 +1,5 @@
 import re
+from typing import Optional
 
 import psycopg2
 
@@ -16,43 +17,46 @@ class PostgresAdapter:
             self.connection.close()
             self.connection = None
 
-    def explain(self, query: str) -> dict:
-        with self.connection.cursor() as cursor:
-            # Postgres might not use an index when there's not much (no) data
-            cursor.execute("SET enable_seqscan = OFF;")
+    def explain(self, query: str) -> Optional[dict]:
+        try:
+            with self.connection.cursor() as cursor:
+                # Postgres might not use an index when there's not much (no) data
+                cursor.execute("SET enable_seqscan = OFF;")
 
-            # TODO: what if a different schema is being used?
-            cursor.execute("SET search_path TO public;")
+                # TODO: what if a different schema is being used?
+                cursor.execute("SET search_path TO public;")
 
-            # replace '%s' placeholders with '$N' where N=1..
-            r = re.compile(r"\%s")
-            n = len(r.findall(query))
+                # replace '%s' placeholders with '$N' where N=1..
+                r = re.compile(r"\%s")
+                n = len(r.findall(query))
 
-            for i in range(1, n + 1):
-                query = query.replace("%s", f"${i}", 1)
+                for i in range(1, n + 1):
+                    query = query.replace("%s", f"${i}", 1)
 
-            # find the number of parameters in the query
-            r = re.compile(r"(\$\d+)")
-            n = len(r.findall(query))
+                # find the number of parameters in the query
+                r = re.compile(r"(\$\d+)")
+                n = len(r.findall(query))
 
-            # `unknown` type for each parameter
-            args = ", ".join(["unknown"] * n)
+                # `unknown` type for each parameter
+                args = ", ".join(["unknown"] * n)
 
-            cursor.execute("SET plan_cache_mode = force_generic_plan;")
+                cursor.execute("SET plan_cache_mode = force_generic_plan;")
 
-            statement = "stmt"
-            if n > 0:
-                statement = f"stmt({args})"
-            cursor.execute(f"PREPARE {statement} AS {query}")
-            nulls = ", ".join(["NULL"] * n)
-            if n > 0:
-                statement = f"stmt({nulls})"
-            explain = f"EXPLAIN (FORMAT JSON) EXECUTE {statement}"
-            cursor.execute(explain)
-            (res,) = cursor.fetchone()
-            cursor.execute("DEALLOCATE stmt;")
+                statement = "stmt"
+                if n > 0:
+                    statement = f"stmt({args})"
+                cursor.execute(f"PREPARE {statement} AS {query}")
+                nulls = ", ".join(["NULL"] * n)
+                if n > 0:
+                    statement = f"stmt({nulls})"
+                explain = f"EXPLAIN (FORMAT JSON) EXECUTE {statement}"
+                cursor.execute(explain)
+                (res,) = cursor.fetchone()
+                cursor.execute("DEALLOCATE stmt;")
 
-            return res[0]
+                return res[0]
+        except psycopg2.errors.UndefinedTable:
+            return None
 
 
 if __name__ == "__main__":
